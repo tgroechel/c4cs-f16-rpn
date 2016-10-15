40409747f8da7bb5f4715dbd1288ec7cb8a26378
tgroeche <tgroeche@umich.edu>
2016-10-15 16:27:53 -0400

    Added ^ test

--- test_rpn.py
+++ test_rpn.py
@@ -22,3 +22,6 @@ class TestBasics(unittest.TestCase):
 	def test_badstring(self):
 		with self.assertRaises(TypeError):
 			rpn.calculate("1 2 3 +")
+	def test_exponent(self):
+		result = rpn.calculate("7 2 ^")
+		self.assertEqual(49)
